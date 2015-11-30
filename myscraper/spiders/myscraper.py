e1aed27375cc6a3b0922ef62ffecdcc382c5ef9f
Fabien Vauchelles <fabien@vauchelles.com>
2015-11-30 11:04:49 +0100

    pagination: skeleton

--- myscraper/spiders/myscraper.py
+++ myscraper/spiders/myscraper.py
@@ -10,11 +10,29 @@ class MyScraper(Spider):
 
     def start_requests(self):
         # First request
-        yield Request(
-            url=u'FILL_WITH_URL',
-            callback=self.parse,
-        )
+
+        # Use a loop
 
 
     def parse(self, response):
-        pass
+        # Find a list of div which contains a person (use CSS)
+        persons_el = response.css('.person')
+
+        # Browse the list
+        for person_el in persons_el:
+
+            # Create a new item
+            item = PersonItem()
+
+            # Extract the name of the person (use CSS + XPath)
+            name_el = person_el.css('.name').xpath('text()').extract()
+            if len(name_el) > 0:
+                item['name'] = name_el[0]
+
+            # Extract the ticket fare of the person (use CSS + XPath)
+            ticket_fare_el = person_el.css('.ticket_fare').xpath('text()').extract()
+            if len(ticket_fare_el) > 0:
+                item['ticket_fare'] = ticket_fare_el[0]
+
+            # Export the item
+            yield item
