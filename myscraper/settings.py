c4504f18f3c0b069f16520d9f820bba4aab5eb6c
Fabien Vauchelles <fabien@vauchelles.com>
2015-11-30 11:08:17 +0100

    useragent: soluce

--- myscraper/settings.py
+++ myscraper/settings.py
@@ -4,3 +4,5 @@ BOT_NAME = 'myscraper'
 
 SPIDER_MODULES = ['myscraper.spiders']
 NEWSPIDER_MODULE = 'myscraper.spiders'
+
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
